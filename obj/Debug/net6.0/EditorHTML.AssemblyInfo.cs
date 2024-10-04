f82a692a71070d60020e93e38cab48f2dd62ac85
Elioenai.Gonçalves <elioenai.andrade@hotmail.com>
2024-10-03 23:45:27 -0300

    add config dockerfile

--- obj/Debug/net6.0/EditorHTML.AssemblyInfo.cs
+++ obj/Debug/net6.0/EditorHTML.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2f1c78923a4e5723221ba750a0a55760b83c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41355b9340feb3fa625c1de113655c50e546434b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHTML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
